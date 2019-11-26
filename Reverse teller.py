23d9110e80a3b395fadc01e5126b75323ccedc5c
Ayush925 <49468808+Ayush925@users.noreply.github.com>
2019-11-26 22:16:13 +0530

    Add files via upload

---
+++ Reverse teller.py
@@ -0,0 +1,12 @@
+a: str = input('Enter any text.\n')
+b = len(a)
+i: int = 0
+list = []
+while i != b:
+    i = i + 1
+    list.append(a[i])
+    # print(list[i])
+# print('\f{}'a[b])
+# b = b-1
+# print(list)
+list.index()
